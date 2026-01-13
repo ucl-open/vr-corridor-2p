53b01b02ec69c2e351a027b51a1d8590162d1358
RoboDoig <erskine94@gmail.com>
2026-01-13 10:25:50 +0000

    Initial schema definition for task structure

--- src/ucl_open_vr_corridor_2p/task.py
+++ src/ucl_open_vr_corridor_2p/task.py
@@ -1,5 +1,5 @@
 # Import core types
-from typing import Literal
+from typing import Literal, Optional, List
 from pydantic import Field
 
 from swc.aeon.io import reader
@@ -7,9 +7,25 @@ from swc.aeon.schema import BaseSchema, data_reader
 
 from ucl_open_vr_corridor_2p import __semver__
 
+class TaskParameters(BaseSchema):
+    rng_seed: Optional[float] = Field(default=None, description="Seed of the random number generator")
+    
+class Landmark(BaseSchema):
+    size: float = Field(default=1, description="This landmark's size in VR space.")
+    texture: str
+    reward_valence: int = Field(default=0, description="Flag describing this landmark's reward valence.")
+    
+class Trial(BaseSchema):
+    landmarks: List[List[Landmark]]
+    
+class Block(BaseSchema):
+    available_trials: List[Trial]
+    max_trials: int = Field(default=10, ge=1)
+    
+
 # TODO - should inherit from some TaskParameters base class rather than BaseSchema
 class UclOpenVrCorridor2pTaskParameters(BaseSchema):
-    ...
+    blocks: List[Block]
 
 
 class UclOpenVrCorridor2pTaskLogic(BaseSchema):
