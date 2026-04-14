ad91fa2be0a946ffe2d5db106806af743c8cb531
ssriranga <srirangasonali@gmail.com>
2026-04-14 10:54:31 +0100

    swaps across movement source
    - this is similar to main

--- examples/rig.py
+++ examples/rig.py
@@ -93,8 +93,8 @@ rig = UclOpenVrCorridor2pRig(
     quad_time_lower_bound=0.2,
     quad_time_upper_bound=0.5,
     #movement_source=MouseWheelMovementSource(source_type="mouse_wheel", gain=0.1) # computer mouse wheel
-    #movement_source=SensorMovementSource(source_type="sensor_movement") # encoder 
-    movement_source=PlaybackMovementSource(source_type="playback", file_path="C:/CODE/BONSAI/vr-corridor-2p/temp_data/sub-PlaybackTest/ses-PlaybackTest_date-2026-04-13T10-57-27/RenderFrameCount/RenderFrameCount.csv", index=4)
+    movement_source=SensorMovementSource(source_type="sensor_movement") # encoder 
+    #movement_source=PlaybackMovementSource(source_type="playback", file_path="C:/CODE/BONSAI/vr-corridor-2p/temp_data/sub-PlaybackTest/ses-PlaybackTest_date-2026-04-13T10-57-27/RenderFrameCount/RenderFrameCount.csv", index=4)
 )
 
 def main(path_seed: str = "./local/{schema}.json"):
