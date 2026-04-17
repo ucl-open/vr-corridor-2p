553d9f43567a8d54c810de40e3edb2a090b99d9e
ssriranga <srirangasonali@gmail.com>
2026-04-17 09:53:30 +0100

    checks for end of corridor signal after 500ms delay

--- examples/session.py
+++ examples/session.py
@@ -10,8 +10,8 @@ session = UclOpenSession(
     repository_url="https://github.com/ucl-open/vr-corridor-2p",
     #logging_root_path="../temp_data",
     logging_root_path="C:/UCLOpenDATA/",
-    animal_id="M26005/20260416/",
-    session_id="M26005_BaselineCorridor_OpenLoop2_20260416_00001" 
+    animal_id="M26005/20260417/",
+    session_id="M26005_BaselineCorridor_OpenLoopReplay_20260417_00001" 
 )   
 
 def main(path_seed: str = "./local/{schema}.json"):
